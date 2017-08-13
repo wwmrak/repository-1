ac055daa259b9fe9a4c3da64a6a2a8f888843a24
Mrak <Mrak@Mrak-PC.fritz.box>
2017-08-13 03:59:44 +0200

    Refactored class.

---
+++ JSF-EJB-JPA-Wildfly-Employee-Management/src/ejb/services/AccessDatabase.java
@@ -0,0 +1,13 @@
+package ejb.services;
+
+import java.util.List;
+import jpa.entities.PersonalInfo;
+
+public interface AccessDatabase {
+	public void createRecord(String name, String surname, String address, String education, String department); 
+	public void deleteRecord(String name, String surname);	
+	public void changeRecord(String name, String surname, String address, String education, String department);	
+	public List<PersonalInfo> selectWholeTable();	
+	public List<Integer> departmentsDisplay();
+
+}

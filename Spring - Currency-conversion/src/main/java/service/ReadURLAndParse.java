954a797754e6e6aa0ee8cb8d991026a6aeb974b7
Mrak <Mrak@Mrak-PC.fritz.box>
2017-08-15 00:44:29 +0200

    Refactored class.

--- Spring - Currency-conversion/src/main/java/service/ReadURLAndParse.java
+++ Spring - Currency-conversion/src/main/java/service/ReadURLAndParse.java
@@ -1,32 +1,39 @@
 package service;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ReadURLAndParse {
-	private static List<ArrayList<String>> listOfRecords = new ArrayList<ArrayList<String>>();
+	private List<Map<String, String>> allDaysCurrencyInfoList = new ArrayList<Map<String, String>>();
 
 	public static void main(String[] args) {
-//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("SpringContext.xml");
+		new ReadURLAndParse().startProgram();
+	}
+	
+	public void startProgram() {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("SpringContext.xml");
 		
 		new ReadURLAndParse().getDownloadUrls();
 
-//		AccessDatabase accessDatabaseObj = (AccessDatabase) context.getBean("createTable");
-//		accessDatabaseObj.createTableTecajneListe(listOfRecords);
+		AccessDatabase accessDatabaseObj = (AccessDatabase) context.getBean("accessDatabase");
+		accessDatabaseObj.createTableCurrencyRates(allDaysCurrencyInfoList);
 
-//		new CurrencyConversion().currencyConversion(accessDatabaseObj);
+		new CurrencyConversion().currencyConversion(accessDatabaseObj);
 
-//		accessDatabaseObj.createRazlikovnaTablica();
+		accessDatabaseObj.createDifferenceTable();
 	}
 
 	public void getDownloadUrls() {
@@ -49,51 +56,64 @@ public class ReadURLAndParse {
 	}
 	
 	public void download(String url) {
-		String line;
 		try {
 			URL urlObj = new URL(url);
 			String dateFromUrl = url.substring(26, 32);
 
-			// working behind firewall: uncomment lines below
-			// Proxy proxy = new Proxy(Proxy.Type.HTTP, new
-			// InetSocketAddress("192.168.0.202", 8080)); 
-			// HttpURLConnection con = (HttpURLConnection)
-			// data.openConnection(proxy); 
-
 			HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+			
 			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			while ((line = in.readLine()) != null) {
-				String[] oneLinePartsArray = line.split("\\s+");
-				List<String> oneLinePartsList = Arrays.asList(oneLinePartsArray);
-
-				//1.array u records list je to
-				//rate designation : 001311220140101201514 (whole row)
-				if (oneLinePartsArray[0].length() > 20) {
-					String rateSheetDesignation = oneLinePartsArray[0];
-					
-					ArrayList<String> rateSheetDesignationList = new ArrayList<String>(); //broj tecajnice
-					rateSheetDesignationList.add(rateSheetDesignation);
-					listOfRecords.add(rateSheetDesignationList);
-					
-					continue;
-				}
-//				036AUD001       5,153033       5,168539       5,184045
-				
-				ArrayList<String> oneCurrencyDailyInfoList = new ArrayList<String>();
-				oneCurrencyDailyInfoList.addAll(oneLinePartsList);
-				oneCurrencyDailyInfoList.add(oneLinePartsArray[0].substring(0, 3));
-				oneCurrencyDailyInfoList.add(oneLinePartsArray[0].substring(3, 6));
-				oneCurrencyDailyInfoList.add(oneLinePartsArray[0].substring(6, 9));
-				oneCurrencyDailyInfoList.add(dateFromUrl);
-
-				listOfRecords.add(oneCurrencyDailyInfoList);
-			}
+			parseDocumentLine(dateFromUrl, in);
+			
 			in.close();
 			con.disconnect();
 			return;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void parseDocumentLine(String dateFromUrl, BufferedReader in)  {
+		String line;
+		try {
+			while ((line = in.readLine()) != null) {
+				String[] oneLinePartsArray = line.split("\\s+");
+
+				if (oneLinePartsArray[0].length() > 20) {
+					String rateSheetDesignation = oneLinePartsArray[0];
+					
+					Map<String, String> rateSheetDesignationMap = new HashMap<String, String>(); 
+					rateSheetDesignationMap.put("rateSheetDesignation", rateSheetDesignation);
+					allDaysCurrencyInfoList.add(rateSheetDesignationMap);
+					
+					continue;
+				}
+				
+				setLinePartsInList(oneLinePartsArray, dateFromUrl);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	void setLinePartsInList(String[] oneLinePartsArray, String dateFromUrl) {
+		String currencyDesignation = oneLinePartsArray[0].substring(0, 3);
+		String currencyCode = oneLinePartsArray[0].substring(3, 6);
+		String currencyDesignation2 = oneLinePartsArray[0].substring(6, 9);
+		String buyingCurrencyRate = oneLinePartsArray[1];
+		String middleCurrencyRate = oneLinePartsArray[2];
+		String sellingCurrencyRate = oneLinePartsArray[3];
+		
+		Map<String, String> oneCurrencyDailyInfoMap = new HashMap<String, String>();
+		oneCurrencyDailyInfoMap.put("currencyCode", currencyCode);
+		oneCurrencyDailyInfoMap.put("currencyDesignation", currencyDesignation);
+		oneCurrencyDailyInfoMap.put("currencyDesignation2", currencyDesignation2);
+		oneCurrencyDailyInfoMap.put("buyingCurrencyRate", buyingCurrencyRate);
+		oneCurrencyDailyInfoMap.put("middleCurrencyRate", middleCurrencyRate);
+		oneCurrencyDailyInfoMap.put("sellingCurrencyRate", sellingCurrencyRate);
+		oneCurrencyDailyInfoMap.put("dateFromUrl", dateFromUrl);
+
+		allDaysCurrencyInfoList.add(oneCurrencyDailyInfoMap);
 	}
 	
 	void setCalendarStartAndEnd(Calendar calendarStart, Calendar calendarEnd) {

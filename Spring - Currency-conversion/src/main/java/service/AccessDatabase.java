bcd45a6c1cf84af080a6abe1ce43908989e6c096
Mrak <Mrak@Mrak-PC.fritz.box>
2017-08-15 00:36:31 +0200

    Refactored class.

--- Spring - Currency-conversion/src/main/java/service/AccessDatabase.java
+++ Spring - Currency-conversion/src/main/java/service/AccessDatabase.java
@@ -5,7 +5,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import entity.DifferenceTable;
@@ -16,89 +19,89 @@ public class AccessDatabase {
 	@Autowired
 	RegistrationBean registrationBeanObj;
 		
-	public void createTableTecajneListe(List<ArrayList<String>> listOfRecords) {
-		String brojTecajnice = "000";
-		Date datumPrimjene = null;
+	public void createTableCurrencyRates(List<Map<String, String>> allDaysCurrencyInfoList) {
+		String rateSheetDesignation = "";
+		Date applicationDate = null;
 
-		for (ArrayList<String> oneRecord : listOfRecords) {
-			if (oneRecord.size() < 2) {
-				brojTecajnice = oneRecord.get(0);
+		for (Map<String, String> oneDayCurrencyInfoMap : allDaysCurrencyInfoList) {
+			if (oneDayCurrencyInfoMap.size() < 2) {
+				rateSheetDesignation = oneDayCurrencyInfoMap.get("rateSheetDesignation");
 				continue;
 			}
 
 			try {
-				datumPrimjene = new SimpleDateFormat("ddMMyy").parse(oneRecord.get(7));
+				applicationDate = new SimpleDateFormat("ddMMyy").parse(oneDayCurrencyInfoMap.get("dateFromUrl"));
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
 
 			Calendar cal = Calendar.getInstance();
-			cal.setTime(datumPrimjene);
+			cal.setTime(applicationDate);
 			cal.add(Calendar.DATE, -1);
-			Date datumIzrade = cal.getTime();
+			Date creationDate = cal.getTime();
 
 			CurrencyRates currencyRatesObj = new CurrencyRates();
-			currencyRatesObj.setRateDesignation(Integer.parseInt(brojTecajnice.substring(0, 3)));
-			currencyRatesObj.setCreationDate(datumIzrade);
-			currencyRatesObj.setApplicationDate(datumPrimjene);
-			currencyRatesObj.setCurrencyNumber(Integer.parseInt(oneRecord.get(4)));
-			currencyRatesObj.setCurrencyCode(oneRecord.get(5));
-			currencyRatesObj.setNumberOfUnits(Integer.parseInt(oneRecord.get(6)));
-			currencyRatesObj.setBuyingRate(Double.parseDouble(oneRecord.get(1).replace(',', '.')));
-			currencyRatesObj.setMiddleRate(Double.parseDouble(oneRecord.get(2).replace(',', '.')));
-			currencyRatesObj.setSellingRate(Double.parseDouble(oneRecord.get(3).replace(',', '.')));
+			fillCurrencyRatesProperties(rateSheetDesignation, applicationDate, oneDayCurrencyInfoMap, creationDate,
+					currencyRatesObj);
 			
-			System.out.println(registrationBeanObj);
 			registrationBeanObj.getCurrencyRatesRepository().save(currencyRatesObj);
 		}
+	}
+
+	private void fillCurrencyRatesProperties(String rateSheetDesignation, Date applicationDate,
+			Map<String, String> oneDayCurrencyInfoMap, Date creationDate, CurrencyRates currencyRatesObj) {
+		currencyRatesObj.setRateDesignation(Integer.parseInt(rateSheetDesignation.substring(0, 3)));
+		currencyRatesObj.setCreationDate(creationDate);
+		currencyRatesObj.setApplicationDate(applicationDate);
+		currencyRatesObj.setCurrencyNumber(Integer.parseInt(oneDayCurrencyInfoMap.get("currencyDesignation")));
+		currencyRatesObj.setCurrencyCode(oneDayCurrencyInfoMap.get("currencyCode"));
+		currencyRatesObj.setNumberOfUnits(Integer.parseInt(oneDayCurrencyInfoMap.get("numberOfUnits")));
+		currencyRatesObj.setBuyingRate(Double.parseDouble(oneDayCurrencyInfoMap.get("buyingCurrencyRate").replace(',', '.')));
+		currencyRatesObj.setMiddleRate(Double.parseDouble(oneDayCurrencyInfoMap.get("middleCurrencyRate").replace(',', '.')));
+		currencyRatesObj.setSellingRate(Double.parseDouble(oneDayCurrencyInfoMap.get("sellingCurrencyRate").replace(',', '.')));
 	}
 	
 	public List<List<Double>> conversionQuery(String applicationDate, String currencyCodeEnding,
 			String currencyCodeStarting) {
-		List<Double> listSrednjiTecajValute1 = null;
-		List<Double> listSrednjiTecajValute2 = null;
+		List<Double> middleRateOfFirstCurrencyList = null;
+		List<Double> middleRateOfSecondCurrencyList = null;
 		
 		try {
-		listSrednjiTecajValute1 = registrationBeanObj.getCurrencyRatesRepository().srednjiTecajPoDatumuIValuti(currencyCodeStarting, 
+		middleRateOfFirstCurrencyList = registrationBeanObj.getCurrencyRatesRepository().getMiddleRateByDateAndCurrency(currencyCodeStarting, 
 				new SimpleDateFormat("ddMMyy").parse(applicationDate));
-		listSrednjiTecajValute2 = registrationBeanObj.getCurrencyRatesRepository().srednjiTecajPoDatumuIValuti(currencyCodeEnding, 
+		middleRateOfSecondCurrencyList = registrationBeanObj.getCurrencyRatesRepository().getMiddleRateByDateAndCurrency(currencyCodeEnding, 
 				new SimpleDateFormat("ddMMyy").parse(applicationDate));
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 
 		List<List<Double>> listSrednjiTecaj2Valute = new ArrayList<List<Double>>();
-		listSrednjiTecaj2Valute.add(listSrednjiTecajValute1);
-		listSrednjiTecaj2Valute.add(listSrednjiTecajValute2);
+		listSrednjiTecaj2Valute.add(middleRateOfFirstCurrencyList);
+		listSrednjiTecaj2Valute.add(middleRateOfSecondCurrencyList);
 
 		return listSrednjiTecaj2Valute;
 	}
 
-	public void createRazlikovnaTablica() {
-		
-		List<Object[]> listaRazlikovnaTablica = registrationBeanObj.getCurrencyRatesRepository().selectOznakaValuteDatumPrimjeneSrednjiTecaj();
+	public void createDifferenceTable() {
+		List<Object[]> differenceRatesTablesList = registrationBeanObj.getCurrencyRatesRepository().selectCurrencyCodeApplicationDateAndMiddleRate();
 
-		System.out.println(listaRazlikovnaTablica.size());
-
-		for (int i = 0; i < listaRazlikovnaTablica.size(); i++) {
+		for (int i = 0; i < differenceRatesTablesList.size(); i++) {
 			DifferenceTable differenceTableRow = new DifferenceTable();
-			differenceTableRow.setApplicationDate((Date) listaRazlikovnaTablica.get(i)[0]);
-			differenceTableRow.setApplicationDate((Date) listaRazlikovnaTablica.get(i)[0]);
-			differenceTableRow.setCurrencyCode((String) listaRazlikovnaTablica.get(i)[1]);
+			differenceTableRow.setApplicationDate((Date) differenceRatesTablesList.get(i)[0]);
+			differenceTableRow.setCurrencyCode((String) differenceRatesTablesList.get(i)[1]);
 			if (i < 13)
 				differenceTableRow.setMiddleRateDifference(0);
 			else
-				differenceTableRow.setMiddleRateDifference((Double) listaRazlikovnaTablica.get(i)[2] - (Double) listaRazlikovnaTablica.get(i - 13)[2]);
+				differenceTableRow.setMiddleRateDifference((Double) differenceRatesTablesList.get(i)[2] - (Double) differenceRatesTablesList.get(i - 13)[2]);
 
 			registrationBeanObj.getDifferenceTableRepository().save(differenceTableRow);
 		}
 	}
 	
-	public List<Date> provjeraDatumaIzTablice() {
-		List<Date> listaDatumaPrimjene = registrationBeanObj.getCurrencyRatesRepository().selectDatumPrimjeneFromTecajneListe();
-		System.out.println(listaDatumaPrimjene);
+	public List<Date> checkDatesFromTableCurrencyRates() {
+		List<Date> applicationDatesList = registrationBeanObj.getCurrencyRatesRepository().selectApplicationDateFromCurrencyRatesTable();
 		
-		return listaDatumaPrimjene;
+		return applicationDatesList;
 	}
 	
 	public RegistrationBean getRegistrationBean() {

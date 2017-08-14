cfb9c6d41833e8c8f1d869a7169f1d0028ae656c
Mrak <Mrak@Mrak-PC.fritz.box>
2017-08-14 15:09:47 +0200

    Refactored class.

--- Spring - Currency-conversion/src/main/java/service/ReadURLAndParse.java
+++ Spring - Currency-conversion/src/main/java/service/ReadURLAndParse.java
@@ -3,8 +3,6 @@ package service;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -13,36 +11,96 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Scanner;
-
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ReadURLAndParse {
-
 	private static List<ArrayList<String>> listOfRecords = new ArrayList<ArrayList<String>>();
 
 	public static void main(String[] args) {
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("SpringContext.xml");
 		
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("SpringContext.xml");
-		
-		donwloadAndParse();
+		new ReadURLAndParse().getDownloadUrls();
 
-		CreateTable createTable = (CreateTable) context.getBean("createTable");
-		
-		createTable.createTableTecajneListe(listOfRecords);
+//		AccessDatabase accessDatabaseObj = (AccessDatabase) context.getBean("createTable");
+//		accessDatabaseObj.createTableTecajneListe(listOfRecords);
 
-		new CurrencyConversion().currencyConversion(createTable);
+//		new CurrencyConversion().currencyConversion(accessDatabaseObj);
 
-		createTable.createRazlikovnaTablica();
+//		accessDatabaseObj.createRazlikovnaTablica();
 	}
 
-	public static void donwloadAndParse() {
-
+	public void getDownloadUrls() {
 		SimpleDateFormat formatter = new SimpleDateFormat("ddMMyy");
+		
+		Calendar calendarStart = Calendar.getInstance();
+		Calendar calendarEnd = Calendar.getInstance();
+		setCalendarStartAndEnd(calendarStart, calendarEnd);
 
+		for (Date date = calendarStart.getTime(); calendarStart.before(calendarEnd); calendarStart.add(Calendar.DATE, 1), date = calendarStart.getTime()) {
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(date);
+			if ((calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY)
+					|| (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY))
+				continue;
+
+			String url = "http://www.hnb.hr/tecajn/f" + formatter.format(date) + ".dat";
+			download(url);
+		}
+	}
+	
+	public void download(String url) {
+		String line;
+		try {
+			URL urlObj = new URL(url);
+			String dateFromUrl = url.substring(26, 32);
+
+			// working behind firewall: uncomment lines below
+			// Proxy proxy = new Proxy(Proxy.Type.HTTP, new
+			// InetSocketAddress("192.168.0.202", 8080)); 
+			// HttpURLConnection con = (HttpURLConnection)
+			// data.openConnection(proxy); 
+
+			HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			while ((line = in.readLine()) != null) {
+				String[] oneLinePartsArray = line.split("\\s+");
+				List<String> oneLinePartsList = Arrays.asList(oneLinePartsArray);
+
+				//1.array u records list je to
+				//rate designation : 001311220140101201514 (whole row)
+				if (oneLinePartsArray[0].length() > 20) {
+					String rateSheetDesignation = oneLinePartsArray[0];
+					
+					ArrayList<String> rateSheetDesignationList = new ArrayList<String>(); //broj tecajnice
+					rateSheetDesignationList.add(rateSheetDesignation);
+					listOfRecords.add(rateSheetDesignationList);
+					
+					continue;
+				}
+//				036AUD001       5,153033       5,168539       5,184045
+				
+				ArrayList<String> oneCurrencyDailyInfoList = new ArrayList<String>();
+				oneCurrencyDailyInfoList.addAll(oneLinePartsList);
+				oneCurrencyDailyInfoList.add(oneLinePartsArray[0].substring(0, 3));
+				oneCurrencyDailyInfoList.add(oneLinePartsArray[0].substring(3, 6));
+				oneCurrencyDailyInfoList.add(oneLinePartsArray[0].substring(6, 9));
+				oneCurrencyDailyInfoList.add(dateFromUrl);
+
+				listOfRecords.add(oneCurrencyDailyInfoList);
+			}
+			in.close();
+			con.disconnect();
+			return;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	void setCalendarStartAndEnd(Calendar calendarStart, Calendar calendarEnd) {
 		Date startDate = null;
 		Date endDate = null;
-
+		SimpleDateFormat formatter = new SimpleDateFormat("ddMMyy");
+		
 		try {
 			startDate = formatter.parse("010115");
 			endDate = formatter.parse("310115");
@@ -50,76 +108,7 @@ public class ReadURLAndParse {
 			e.printStackTrace();
 		}
 
-		Calendar start = Calendar.getInstance();
-		start.setTime(startDate);
-		Calendar end = Calendar.getInstance();
-		end.setTime(endDate);
-
-		for (Date date = start.getTime(); start.before(end); start.add(Calendar.DATE, 1), date = start.getTime()) {
-			Calendar cal = Calendar.getInstance();
-			cal.setTime(date);
-			if ((cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY)
-					|| (cal.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY))
-				continue;
-
-			String url = "http://www.hnb.hr/tecajn/f" + formatter.format(date) + ".dat";
-			dataDownloadAndParse(url);
-		}
-	}
-
-	
-	
-	
-	
-	public static void dataDownloadAndParse(String url) {
-
-		String inputLine;
-
-		try {
-			URL data = new URL(url);
-
-			String datumPrimjeneFromUrl = url.substring(26, 32);
-
-			// working behind firewall: unkomentirat donje linije
-			// Proxy proxy = new Proxy(Proxy.Type.HTTP, new
-			// InetSocketAddress("192.168.0.202", 8080)); */
-			// HttpURLConnection con = (HttpURLConnection)
-			// data.openConnection(proxy); */
-
-			HttpURLConnection con = (HttpURLConnection) data.openConnection();
-
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-			while ((inputLine = in.readLine()) != null) {
-
-				String[] record = inputLine.split("\\s+");
-
-				if (record[0].length() > 20) {
-					ArrayList<String> brojTecajnice = new ArrayList<String>();
-					brojTecajnice.add(record[0]);
-					listOfRecords.add(brojTecajnice);
-					continue;
-				}
-
-				List<String> ListTecajInit = Arrays.asList(record);
-
-				ArrayList<String> listTecajDaily = new ArrayList<String>();
-
-				listTecajDaily.addAll(ListTecajInit);
-
-				listTecajDaily.add(record[0].substring(0, 3));
-				listTecajDaily.add(record[0].substring(3, 6));
-				listTecajDaily.add(record[0].substring(6, 9));
-				listTecajDaily.add(datumPrimjeneFromUrl);
-
-				listOfRecords.add(listTecajDaily);
-			}
-			in.close();
-			con.disconnect();
-			return;
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		calendarStart.setTime(startDate);
+		calendarEnd.setTime(endDate);
 	}
 }
